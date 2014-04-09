37c3072d12a49ae8da0a901d2125bc1bdf1d5f90
Mark Rickert <mjar81@gmail.com>
2014-04-09 14:44:56 -0400

    Remove unnecessary styles.

--- app/_stylesheets/talks.rb
+++ app/_stylesheets/talks.rb
@@ -8,14 +8,4 @@ Teacup::Stylesheet.new(:talks) do
     width: '100%',
     top: 60
 
-  style :table_view,
-    separatorStyle: UITableViewCellSeparatorStyleSingleLine,
-    separatorColor: '#c6c6c6'.uicolor,
-    constraints: [
-      :full_width,
-      constrain_below(:header_view).minus(10),
-      constrain(:height).equals(:superview, :height).minus(63)
-    ]
-  style :table_view_background,
-    backgroundColor: '#f0f0f0'.uicolor
 end
