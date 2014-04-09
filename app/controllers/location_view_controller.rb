3b5727544aa8a7c34e0484462dc08a250ca99fe6
Mark Rickert <mjar81@gmail.com>
2014-04-09 16:08:38 -0400

    These two VCs were to similar... make them inherit from a single controller.

--- app/controllers/location_view_controller.rb
+++ app/controllers/location_view_controller.rb
@@ -1,78 +1,17 @@
-class LocationViewController < GenericScreen
+class LocationViewController < InspectMapScreen
   stylesheet :location
   title 'The Location'
 
-  layout :root do |v|
-    @map = subview(MKMapView, :map)
-    @map.delegate = self
-    @map.zoomEnabled = false
-    @map.scrollEnabled = false
-    @map.mapType = MKMapTypeHybrid
-    @map.region = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.8078, -122.4297), MKCoordinateSpanMake(0.02, 0.02))
-
-    @map_height_constraint = Teacup::Constraint.new(@map, :height).equals(100).nslayoutconstraint
-    @map.addConstraint(@map_height_constraint)
-
-    @map_tap = UITapGestureRecognizer.alloc.initWithTarget(self, action: "map_tapped:")
-    @map.addGestureRecognizer(@map_tap)
-
-    @open_button = subview(UIButton.custom, :open_button)
-    default_image = 'button-ticket-active.png'.uiimage
-    @open_button.addConstraint Teacup::Constraint.new(@open_button, :width).equals(default_image.size.width).nslayoutconstraint
-    @open_button.addConstraint Teacup::Constraint.new(@open_button, :height).equals(default_image.size.height).nslayoutconstraint
-    @open_button_top_constraint = Teacup::Constraint.new(@open_button, :top).equals(@map, :bottom).nslayoutconstraint
-    v.addConstraint @open_button_top_constraint
-
-    @open_button.titleLabel.font = 'Arvo'.uifont(11)
-    @open_button.setBackgroundImage(default_image, forState: :normal.uicontrolstate)
-    @open_button.setTitleColor(UIColor.whiteColor, forState: :normal.uicontrolstate)
-
-    @open_button.on(:touch) do
-      self.button_tapped(self)
-    end
-
-    subview(UIImageView, :arrow)
-    @scroll = subview(UIScrollView, :content) do
-      subview(MTLabel, :title)
-      subview(UIView, :line)
-      subview(UILabel, :description)
-    end
-
-    self.navigationController.navigationBar.translucent = false
-    self.automaticallyAdjustsScrollViewInsets = false
-    self.edgesForExtendedLayout = UIRectEdgeNone
+  def lat
+    37.8078
   end
 
-  def mapViewDidFinishLoadingMap(map)
-    @pin = MKPointAnnotation.alloc.init
-    @pin.coordinate = @map.region.center
-    @pin.title = 'Fort Mason, San Francisco'
-    @map.addAnnotation @pin
+  def lon
+    -122.4297
   end
 
-  def mapView(map, viewForAnnotation: annotation)
-    MKPinAnnotationView.alloc.initWithAnnotation(annotation, reuseIdentifier: 'pin')
+  def pin_title
+    'Fort Mason, San Francisco'
   end
 
-  def map_tapped(sender)
-    UIView.animateWithDuration(0.5, animations: lambda do
-      if @map_height_constraint.constant == 100
-        @map_height_constraint.constant = 360
-        @open_button_top_constraint.constant = -50
-      else
-        @map_height_constraint.constant = 100
-        @open_button_top_constraint.constant = 0
-      end
-      self.view.setNeedsUpdateConstraints
-      self.view.layoutIfNeeded
-    end)
-  end
-
-  def button_tapped(sender)
-    "http://maps.apple.com/?ll=37.8078,-122.4297&spn=0.01&q=Fort%20Mason,%20San%20Francisco".nsurl.open
-  end
-
-  def viewDidLayoutSubviews
-    @scroll.contentSize = CGSizeMake(320, 400)
-  end
 end
--- app/controllers/party_view_controller.rb
+++ app/controllers/party_view_controller.rb
@@ -1,79 +1,20 @@
-class PartyViewController < GenericScreen
+class PartyViewController < InspectMapScreen
   stylesheet :party
   title 'The After-Party'
 
-  layout :root do |v|
-    @map = subview(MKMapView, :map)
-    @map.delegate = self
-    @map.zoomEnabled = false
-    @map.scrollEnabled = false
-    @map.mapType = MKMapTypeHybrid
-
-    @map.region = MKCoordinateRegionMake(CLLocationCoordinate2DMake(50.848418, 4.353877), MKCoordinateSpanMake(0.001, 0.001))
-
-    @map_height_constraint = Teacup::Constraint.new(@map, :height).equals(100).nslayoutconstraint
-    @map.addConstraint(@map_height_constraint)
-
-    @map_tap = UITapGestureRecognizer.alloc.initWithTarget(self, action: "map_tapped:")
-    @map.addGestureRecognizer(@map_tap)
-
-    @open_button = subview(UIButton.custom, :open_button)
-    default_image = 'button-ticket-active.png'.uiimage
-    @open_button.addConstraint Teacup::Constraint.new(@open_button, :width).equals(default_image.size.width).nslayoutconstraint
-    @open_button.addConstraint Teacup::Constraint.new(@open_button, :height).equals(default_image.size.height).nslayoutconstraint
-    @open_button_top_constraint = Teacup::Constraint.new(@open_button, :top).equals(@map, :bottom).nslayoutconstraint
-    v.addConstraint @open_button_top_constraint
-
-    @open_button.titleLabel.font = 'Arvo'.uifont(11)
-    @open_button.setBackgroundImage(default_image, forState: :normal.uicontrolstate)
-    @open_button.setTitleColor(UIColor.whiteColor, forState: :normal.uicontrolstate)
-
-    @open_button.on(:touch) do
-      self.button_tapped(self)
-    end
-
-    subview(UIImageView, :arrow)
-    @scroll = subview(UIScrollView, :content) do
-      subview(MTLabel, :title)
-      subview(UIView, :line)
-      subview(UILabel, :description)
-    end
-
-    self.navigationController.navigationBar.translucent = false
-    self.automaticallyAdjustsScrollViewInsets = false
-    self.edgesForExtendedLayout = UIRectEdgeNone
+  def lat
+    50.848418
   end
 
-  def mapViewDidFinishLoadingMap(map)
-    @pin = MKPointAnnotation.alloc.init
-    @pin.coordinate = @map.region.center
-    @pin.title = "Delirium Café"
-    @map.addAnnotation @pin
+  def lon
+    4.353877
   end
 
-  def mapView(map, viewForAnnotation: annotation)
-    MKPinAnnotationView.alloc.initWithAnnotation(annotation, reuseIdentifier: 'pin')
+  def pin_title
+    'Delirium Café'
   end
 
-  def map_tapped(sender)
-    UIView.animateWithDuration(0.5, animations: lambda do
-      if @map_height_constraint.constant == 100
-        @map_height_constraint.constant = 360
-        @open_button_top_constraint.constant = -50
-      else
-        @map_height_constraint.constant = 100
-        @open_button_top_constraint.constant = 0
-      end
-      self.view.setNeedsUpdateConstraints
-      self.view.layoutIfNeeded
-    end)
-  end
-
-  def button_tapped(sender)
-    "https://maps.apple.com/?ll=50.848418,4.353877&q=Delirium+Cafe,Impasse%20de%20la%20Fid%C3%A9lit%C3%A9%204,+1000+Brussels,+Belgium".nsurl.open
-  end
-
-  def viewDidLayoutSubviews
-    @scroll.contentSize = CGSizeMake(320, 300)
+  def span
+    0.001
   end
 end
