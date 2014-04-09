3f9328567d17b38ffc09d637357f59a2d1cdc8db
Mark Rickert <mjar81@gmail.com>
2014-04-08 22:29:34 -0400

    New stylesheet 'maps' is superclass for location and party screens.

---
+++ app/_stylesheets/party.rb
@@ -0,0 +1,9 @@
+Teacup::Stylesheet.new(:party) do
+  import :maps
+
+  style :title,
+    text: "Délirium Café"
+
+  style :description,
+    text: "On Friday night you are invited to an awesome after-party at the Délirium Café.\n\nThe Délirium is known for its long beer list, standing at 2,004 different brands in January 2004 as recorded in the The Guinness Book of Records.\n\nThe bar is located in the small alley called Impasse de la Fidélité, only a couple of hundred metres from the Grand-Place. Don't miss the Jeanneke Pis statue nearby!"
+end
