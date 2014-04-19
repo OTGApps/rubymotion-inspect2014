487469341357f802254112ba1ba83b912127f03a
Mark Rickert <mjar81@gmail.com>
2014-04-19 11:41:59 -0400

    Add SDWebImage and CachedImage class

---
+++ app/models/cached_image.rb
@@ -0,0 +1,9 @@
+class CachedImage < UIImageView
+  URL_PREFIX = 'https://raw.githubusercontent.com/MohawkApps/rubymotion-inspect2014/master/resources/'
+  attr_accessor :placeholder
+
+  def set_image(i)
+    image_url = "#{URL_PREFIX}#{i}".nsurl
+    setImageWithURL(image_url, placeholderImage:placeholder.uiimage)
+  end
+end
