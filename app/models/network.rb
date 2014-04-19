19671d312abd6fb7d744a290c0ed76a0a2ebfdbd
Mark Rickert <mjar81@gmail.com>
2014-04-19 12:35:12 -0400

    cache location and party plists form teh network every app opening.

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,8 +1,11 @@
 class Networking
+  # TODO - Update this url once the PR is accepted
+  #RAW_PATH = 'https://raw.githubusercontent.com/MohawkApps/rubymotion-inspect2014/master/resources/'
+  RAW_PATH = 'https://raw.githubusercontent.com/MohawkApps/rubymotion-inspect2014/location-party-plist/resources/'
+
   def self.cache_data
-    raw_path = 'https://raw.githubusercontent.com/MohawkApps/rubymotion-inspect2014/master/resources/'
-    ['talks.plist', 'speakers.plist', 'sponsors.plist'].each do |cache_file|
-      AFMotion::HTTP.get("#{raw_path}#{cache_file}", q: Time.now.to_i) do |result|
+    ['talks.plist', 'speakers.plist', 'sponsors.plist', 'location.plist', 'party.plist'].each do |cache_file|
+      AFMotion::HTTP.get("#{RAW_PATH}#{cache_file}", q: Time.now.to_i) do |result|
         ap "Caching #{cache_file}" if BW.debug?
         if result && result.body
           path = cache_file.document_path
@@ -12,6 +15,6 @@ class Networking
           "talks_cached".post_notification if cache_file.start_with?('talks')
         end
       end
-    end    
+    end
   end
 end
